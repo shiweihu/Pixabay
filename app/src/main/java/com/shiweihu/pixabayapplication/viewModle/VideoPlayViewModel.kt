288239d953ebc669df54d385171eccd4027f1068
hushiwei <405927035@qq.com>
2022-07-02 19:28:46 +0930

    加入API 26以下的视频保存方案

--- app/src/main/java/com/shiweihu/pixabayapplication/viewModle/VideoPlayViewModel.kt
+++ app/src/main/java/com/shiweihu/pixabayapplication/viewModle/VideoPlayViewModel.kt
@@ -1,12 +1,18 @@
 package com.shiweihu.pixabayapplication.viewModle
 
+import android.content.ClipData
 import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.os.Environment.DIRECTORY_MOVIES
 import android.provider.MediaStore
 import android.view.View
+import androidx.annotation.RequiresApi
+import androidx.core.net.toFile
 import androidx.lifecycle.ViewModel
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.FragmentNavigatorExtras
@@ -23,6 +29,7 @@ import com.shiweihu.pixabayapplication.videoPlayView.VideoPlayActivityArgs
 
 import com.shiweihu.pixabayapplication.videoPlayView.VideoPlayFragment
 import dagger.hilt.android.lifecycle.HiltViewModel
+import java.io.File
 import java.util.*
 import javax.inject.Inject
 
@@ -54,8 +61,54 @@ class VideoPlayViewModel @Inject constructor(
 
 
     fun downloadVideo(context:Context,mediaItem:MediaItem,callBack:()->Unit){
+       if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+           downloadV26(context,mediaItem,callBack)
+       }else{
+           download(context,mediaItem,callBack)
+       }
+
+    }
+    private fun download(context: Context,mediaItem:MediaItem,callBack:()->Unit){
+
+        val path = Environment.getExternalStoragePublicDirectory(DIRECTORY_MOVIES).absolutePath
+        val filePath = path +"/${UUID.randomUUID()}.mp4"
+        val transformer: Transformer = Transformer.Builder(context)
+            .addListener(object: Transformer.Listener{
+                override fun onTransformationCompleted(inputMediaItem: MediaItem,transformationResult: TransformationResult) {
+                    super.onTransformationCompleted(inputMediaItem,transformationResult)
+
+//                    val fileDescriptor = context.contentResolver.openFileDescriptor(outputUri!!,"w")
+//                    val file = File(Environment.getDownloadCacheDirectory().path+"/temp.mp4")
+//                    val uri = Uri.fromFile(file)
+                    val file = File(filePath)
+                    val uri = Uri.fromFile(file)
+                    val intent = Intent(Intent.ACTION_SEND).also {
+                        it.type = "video/*"
+                        it.putExtra(Intent.EXTRA_STREAM, ClipData.Item(uri).uri)
+                    }
+
+                    val chose_intent = Intent.createChooser(intent,context.resources.getString(R.string.app_choser_title))
+                    context.startActivity(chose_intent)
+                    callBack()
+                }
+
+                override fun onTransformationError(
+                    inputMediaItem: MediaItem,
+                    exception: TransformationException
+                ) {
+                    super.onTransformationError(inputMediaItem, exception)
+
+                    exception.printStackTrace()
+                    callBack()
+                }
+            }).build()
+        transformer.startTransformation(mediaItem,filePath)
+    }
+
+    @RequiresApi(26)
+    private fun downloadV26(context: Context,mediaItem:MediaItem,callBack:()->Unit){
         val content = ContentValues()
-        content.put(MediaStore.Video.Media.DISPLAY_NAME,UUID.randomUUID().toString());
+        content.put(MediaStore.Video.Media.DISPLAY_NAME,UUID.randomUUID().toString())
         content.put(MediaStore.Video.Media.MIME_TYPE,"video/mp4")
         val outputUri = context.contentResolver.insert(MediaStore.Video.Media.EXTERNAL_CONTENT_URI,content)
         // Configure and create a Transformer instance.
@@ -92,8 +145,8 @@ class VideoPlayViewModel @Inject constructor(
 
         val fileDescriptor = context.contentResolver.openFileDescriptor(outputUri!!,"w")
         transformer.startTransformation(mediaItem, fileDescriptor!! )
-
     }
+
 
     override fun onCleared() {
         super.onCleared()
